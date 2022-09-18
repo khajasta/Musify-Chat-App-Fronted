5fe94e22880627121caf9f1e0de84017259acb7c
khajasta <i190468@nu.edu.pk>
2022-09-18 04:20:45 -0700

    SMD UIs

---
+++ app/src/main/java/com/khajasta/i190468/eighteenscreen.java
@@ -0,0 +1,14 @@
+package com.khajasta.i190468;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class eighteenscreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_eighteenscreen);
+    }
+}
