2d4f8cd9d49f355aa97bad0b79dcf42363347d4c
khajasta <i190468@nu.edu.pk>
2022-09-18 05:25:08 -0700

    SMD UIs

---
+++ app/src/main/java/com/khajasta/i190468/twentyscreen.java
@@ -0,0 +1,14 @@
+package com.khajasta.i190468;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class twentyscreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_twentyscreen);
+    }
+}
